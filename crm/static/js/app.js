f079fbdbe5c1a11803c101c7ffee4629b2e20b18
Sahil jadhav <sahiljadhav3330@gmail.com>
2024-07-20 12:40:27 +0530

    first commit

---
+++ crm/static/js/app.js
@@ -0,0 +1,11 @@
+
+// message / notification timer
+
+var message_timeout = document.getElementById("message-timer");
+
+setTimeout(function()
+{
+
+   message_timeout.style.display = "none";
+
+}, 5000);
